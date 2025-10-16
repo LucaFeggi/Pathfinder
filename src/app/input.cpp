01c6c44f367eb6e15cc422cc2c31d1fdb3285cb5
LucaFeggi <81522854+LucaFeggi@users.noreply.github.com>
2025-10-16 10:26:32 +0200

    Update input.cpp

--- src/app/input.cpp
+++ src/app/input.cpp
@@ -2,18 +2,32 @@
 
 #include "../config.hpp"
 
-void Input::poll(const SDL_Event &event, bool *commands){
+Input::Input() : command{ false } {}
+
+void Input::poll(const SDL_Event &event){
     switch(event.type){
         case SDL_QUIT:
-            commands[Commands::QUIT] = true;
+            command[Commands::QUIT] = true;
             break;
         case SDL_KEYDOWN:
-            if(event.key.keysym.sym == SDLK_ESCAPE) commands[Commands::QUIT] = true;
+            if(event.key.keysym.sym == SDLK_ESCAPE) set_command_active(Commands::QUIT);
             break;
         case SDL_WINDOWEVENT:
-            if(event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) commands[Commands::RENDER] = true;
+            if(event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) set_command_active(Commands::RENDER);
             break;
         default:
             break;
     }       
+}
+
+bool Input::is_command_active(const Commands comm) const{
+    return command[comm];
+}
+
+void Input::set_command_active(const Commands comm){
+    command[comm] = true;
+}
+
+void Input::set_command_inactive(const Commands comm){
+    command[comm] = false;
 }
